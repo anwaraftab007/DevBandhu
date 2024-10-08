d908da47adc6128e5026e244088ae2f4ca84ab48
anwaraftab007 <anwaraftab007@gmail.com>
2024-10-09 03:52:21 +0530

    Initial setup complete..

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen bg-[#35374B] flex justify-center items-center'>
+      <h1 className="text-4xl text-white">
+        DevBandhu
+      </h1>
+    </div>
+  )
+}
+
+//Dark: #35374B || #393053 || #002233
+//Light: #E7F6F2 || #EEEEEE || edf2fb
+export default App
